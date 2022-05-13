89f656d08582432fd18a50277afef3025cce45b6
Luca Silviu-Catalin <catalinluca01@gmail.com>
2022-05-13 22:28:00 +0300

    Create project hierarchy

---
+++ PasswordManager/DataBaseManager/Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataBaseManager
+{
+    public static class Test
+    {
+        public static string TestDependency()
+        {
+            return "something";
+        }
+    }
+}
---
+++ PasswordManager/SecurityUtility/Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SecurityUtility
+{
+    public static class Test
+    {
+        public static string SecurityTest()
+        {
+            return "hello from security";
+        }
+    }
+}
