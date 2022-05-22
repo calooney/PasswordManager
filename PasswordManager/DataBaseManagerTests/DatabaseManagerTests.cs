f1eb68fc7b5140fcfa0f6f6973967daf0104d225
Luca Silviu-Catalin <catalinluca01@gmail.com>
2022-05-22 20:26:25 +0300

    add unitests for DataabaseManager

---
+++ PasswordManager/DataBaseManagerTests/DatabaseManagerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataBaseManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseManager.Tests
+{
+    [TestClass()]
+    public class DatabaseManagerTests
+    {
+        [TestMethod()]
+        public void GetUserAccountsTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
